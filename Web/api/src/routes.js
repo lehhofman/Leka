130dc20e2c3a82be1a9daeee362d909029a18630
lehhofman <desenvolvimentoleticia2106@gmail.com>
2024-11-19 10:44:35 -0300

    proj

---
+++ Web/api/src/routes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+const Middleware = require('./middleware/middleware');
+const Usuario = require('./controllers/usuario');
+const Resumo = require('./controllers/resumo');
+
+router.post('/signup', Usuario.create);
+router.post('/login', Usuario.login);
+router.get('/perfil', Middleware.validaAcesso, Usuario.perfil);
+router.get('/usuarios', Middleware.validaAcesso, Usuario.read);
+router.get('/usuarios/:id', Middleware.validaAcesso, Usuario.read);
+router.put('/usuarios/:id', Middleware.validaAcesso, Usuario.update);
+router.delete('/usuarios/:id', Middleware.validaAcesso, Usuario.del);
+
+router.get('/resumos', Resumo.getSummaries);
+router.get('/resumos/:id', Resumo.getSummaryById);
+router.post('/resumos',  Resumo.createSummary);
+router.put('/resumos/:id',Resumo.updateSummary);
+router.delete('/resumos/:id',Resumo.deleteSummary);
+
+module.exports = router;
+
+
+router.get('/', (req, res) => { 
+
+    return res.json("API respondendo"); 
+
+});
+
+module.exports = router;
